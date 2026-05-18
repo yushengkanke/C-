66682d782ada0ad81ee37713bc75cfbeda022368
yushengkanke <3324771517@qq.com>
2026-05-18 16:24:22 +0800

    基础部分还未完成

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 //结构体定义（单向链表）
+int total_book_count=0;
 typedef struct book{
     int id;             //计划自动生成（待完成）
     char name[100];     //书名
@@ -10,7 +12,9 @@ typedef struct book{
     int borrowcount;    //借阅次数
     struct book* next;
 } book;
-
+book* head=NULL;
+book* t;//辅助链表操作
+book* end;//指向链表最后的节点；
 void showMenu() {
     printf("====== 图书管理系统 ======\n");
     printf("1. 添加图书\n");
@@ -24,10 +28,68 @@ void showMenu() {
     printf("0. 退出\n");
     printf("请选择: ");
 }
+void addbook(void)//或许应该加一个批量添加书的功能
+{
+    
+    book* bookptr=malloc(sizeof(book));
+    if(bookptr==NULL)
+    {
+        printf("内存分配失败。\n");
+        return ;
+    }
+    else{
+        printf("请输入");
+    }
+}
 int main(void)
 {
     int choice;
+    FILE* fp=fopen("bookdata.txt","r");
+    head=malloc(sizeof(book));
+    if(fp==NULL){
+        total_book_count=0;
+        printf("未找到数据文件，将创建新系统。\n");
+        t=head;
+        end=head;
+    }
+    else
+    {
+        //程序开始时先读入文件中的数据(或许可以写成函数再整个刷新功能？)
+        fscanf(fp,"%d",&total_book_count);
+        for(int i=0;i<total_book_count;i++)
+        {
+            book* s=malloc(sizeof(book));
+            if(s==NULL)
+            {
+                printf("初始化时内存分配失败\n");
+                //写一个free函数
+                return ;
+            }
+            fscanf(fp,"%d %d %d %d",&(s->id),&(s->total),&(s->available),&(s->borrowcount));
+            fgets(fp,100,s->name);
+            fgets(fp,50,s->author);
+            s->next=NULL;
+            end->next=s;
+            end=end->next;
+        }
+    }
+
+
     showMenu();
     scanf("%d",&choice);
+    while(choice!=0)
+    {
+        switch (choice)
+        {
+        case 1:
+            
+            break;
+        
+        default:
+            break;
+        }
+        scanf("%d",&choice);
+    }
+    //结束后应该把链表中的数据写入文件中（未完成）
     return 0;
 }
