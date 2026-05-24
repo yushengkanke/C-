c1aaa396537fa905bf772369d9ef208db35a8493
yushengkanke <3324771517@qq.com>
2026-05-24 09:57:25 +0800

    文件数据的读取和保存（已完成）

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <windows.h>
+#ifndef CP_UTF8
+#define CP_UTF8 65001
+#endif
+//书名作者不能有空格；
+
 //结构体定义（单向链表）
 int total_book_count=0;
 typedef struct book{
@@ -15,6 +21,17 @@ typedef struct book{
 book* head=NULL;
 book* t;//辅助链表操作
 book* end;//指向链表最后的节点；
+void free_book_list(book* t)
+{
+    
+    while(t!=NULL)
+    {
+        book* dell=t;
+        t=t->next;
+        free(dell);
+        
+    }
+}
 void showMenu() {
     printf("====== 图书管理系统 ======\n");
     printf("1. 添加图书\n");
@@ -38,14 +55,34 @@ void addbook(void)//或许应该加一个批量添加书的功能
         return ;
     }
     else{
-        printf("请输入");
+        printf("请输入：(示例：总库存 可借数量 借阅次数 书名{不能有空格} 作者{不能有空格})");
+    
+    int ntotal,navailable,nborrowcount;
+    char nname[100];
+    char nauthor[50];
+    scanf("%d %d %d %s %s",&ntotal,&navailable,&nborrowcount,nname,nauthor);
+    total_book_count++;
+    book* neww=bookptr;
+    neww->id=total_book_count;
+    neww->total=ntotal;
+    neww->available=navailable;
+    neww->borrowcount=nborrowcount;
+    neww->next=NULL;
+    strcpy(neww->name,nname);
+    strcpy(neww->author,nauthor);
+    end->next=neww;
+    end=end->next;
     }
+
 }
 int main(void)
 {
+    SetConsoleOutputCP(CP_UTF8);      // 设置输出代码页为 UTF-8
+    SetConsoleCP(CP_UTF8);            // 设置输入代码页为 UTF-8
     int choice;
     FILE* fp=fopen("bookdata.txt","r");
     head=malloc(sizeof(book));
+    head->next=NULL;
     if(fp==NULL){
         total_book_count=0;
         printf("未找到数据文件，将创建新系统。\n");
@@ -54,6 +91,9 @@ int main(void)
     }
     else
     {
+        end=head;
+        t=head;
+
         //程序开始时先读入文件中的数据(或许可以写成函数再整个刷新功能？)
         fscanf(fp,"%d",&total_book_count);
         for(int i=0;i<total_book_count;i++)
@@ -62,16 +102,17 @@ int main(void)
             if(s==NULL)
             {
                 printf("初始化时内存分配失败\n");
-                //写一个free函数
-                return ;
+                
+                //free_book_list(head);
+                return 0;
             }
-            fscanf(fp,"%d %d %d %d",&(s->id),&(s->total),&(s->available),&(s->borrowcount));
-            fgets(fp,100,s->name);
-            fgets(fp,50,s->author);
+            fscanf(fp,"%d|%d|%d|%d|%[^|]|%[^\n]",&(s->id),&(s->total),&(s->available),&(s->borrowcount),s->name,s->author);
+        
             s->next=NULL;
             end->next=s;
             end=end->next;
         }
+
     }
 
 
@@ -82,7 +123,7 @@ int main(void)
         switch (choice)
         {
         case 1:
-            
+            addbook();
             break;
         
         default:
@@ -90,6 +131,20 @@ int main(void)
         }
         scanf("%d",&choice);
     }
-    //结束后应该把链表中的数据写入文件中（未完成）
+    if(fp!=NULL)
+    fclose(fp);
+    //结束后应该把链表中的数据写入文件中（完成）
+    FILE* fout=fopen("bookdata.txt","w");
+    fprintf(fout,"%d\n",total_book_count);
+    t=head;
+    while(t->next!=NULL)
+    {
+        fprintf(fout,"%d|%d|%d|%d|%s|%s\n",(t->next->id),(t->next->total),(t->next->available),(t->next->borrowcount),t->next->name,t->next->author);
+        
+        t=t->next;
+    }
+    
+    fclose(fout);
+    
     return 0;
 }
